0b4b2c9e155cdd9bd7fba2ad3a2e37417321ad40
karakazeviewview <36731954+karakazeviewview@users.noreply.github.com>
2022-08-27 14:51:55 +0900

    second

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -8,7 +8,19 @@ function check()
             XHR.open("GET", `/posts/${postId}`, true);
             XHR.responseType = "json";
             XHR.send();
+            XHR.onload = () => {
+                if (XHR.status != 200) {
+                    alert(`Error ${XHR.status}: ${XHR.statusText}`);
+                    return null;
+                }
+                const item = XHR.response.post;
+                if (item.checked === true) {
+                    post.setAttribute("data-check", "true");
+                } else if (item.checked === false) {
+                    post.removeAttribute("data-check");
+                }
+            };
         });
     });
 }
-window.addEventListener("load", check);
+setInterval(check, 1000);
